f96efbc9e540e13855f5401408a8722e96d70dcb
SilvioCarlos <silvio.net.ma@gmail.com>
2020-05-18 11:02:00 -0300

    inicializando projeto

---
+++ src/main/java/com/cadastropessoacidade/cadastropessoacidade/CadastropessoacidadeApplication.java
@@ -0,0 +1,13 @@
+package com.cadastropessoacidade.cadastropessoacidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastropessoacidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastropessoacidadeApplication.class, args);
+	}
+
+}
